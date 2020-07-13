878d1f69e88218385eeae41ca3618f63d233d8aa
windowAcer <nyuuroxy@gmail.com>
2020-07-13 14:00:45 +0200

    define api

---
+++ central-system-api/central-sistem-api.sql
@@ -0,0 +1,6 @@
+
+organization	CREATE TABLE `organization` (
+  `id` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `legalEntity` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci	
